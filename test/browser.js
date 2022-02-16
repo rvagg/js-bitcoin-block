36af6e434ddd44e45a7685bf99eb06dc35e86f6a
Rod Vagg <rod@vagg.org>
2022-02-16 12:59:25 +1100

    fix: browser tests working again

--- test/browser.js
+++ test/browser.js
@@ -5,14 +5,14 @@ const test = require('./test')
 const { fromHex } = require('../util')
 
 for (const key of context.keys()) {
-  it(key, async () => {
-  // load .json files to `fixture.data` and corresponding .hex file binary data as `fixture.block`
-    const hash = key.replace(/^.\/([^.]+)\.json/, '$1')
+  const hash = key.replace(/^.\/([^.]+)\.json/, '$1')
+  it(hash, async () => {
+    // load .json files to `fixture.data` and corresponding .hex file binary data as `fixture.block`
     // console.log((await import(`!!raw-loader!./fixtures/${hash}.hex`)).substr(0, 100)
     const data = (await import(`./fixtures/${hash}.json`)).default
     const blockHex = (await import(`!!raw-loader!./fixtures/${hash}.hex`)).default
     const block = fromHex(blockHex.replace(/\n/g, ''))
 
     test(hash, block, data)
-  })
+  }).timeout(5000)
 }
